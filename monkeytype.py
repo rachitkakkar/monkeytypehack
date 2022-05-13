672c1d3d50cca3da99a6279cac1afdc38b8c63ee
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2022-05-13 20:23:57 +0000

    Add files via upload

---
+++ monkeytype.py
@@ -0,0 +1,14 @@
+import pytesseract
+from pyautogui import screenshot
+from pynput.keyboard import Key, Controller
+from time import sleep
+
+sleep(1)
+
+keyboard = Controller()
+screen = screenshot(region=(500, 825, 1920, 300))
+message = pytesseract.image_to_string(screen).replace('\n', ' ')
+
+for c in message:
+    sleep(0.011)
+    keyboard.press(c)
