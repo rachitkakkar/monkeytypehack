bdcb6807f182cf1d1cbe2b05018ffb33842b442b
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2022-07-24 12:36:13 -0500

    Update monkeytype.py

--- monkeytype.py
+++ monkeytype.py
@@ -9,6 +9,8 @@ keyboard = Controller()
 screen = screenshot(region=(500, 825, 1920, 300))
 message = pytesseract.image_to_string(screen).replace('\n', ' ')
 
+if (message[0] == 'l'): message = message[1:]
+
 for c in message:
     sleep(0.011)
     keyboard.press(c)
